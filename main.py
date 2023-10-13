ca18b54c42fefe906fbbf0f1d5e4363d92a218e0
georgebrnv <barinov.egor2013@gmail.com>
2023-10-13 17:40:23 -0400

    Implement list comprehension

--- main.py
+++ main.py
@@ -18,11 +18,7 @@ while len(guessed_states) < 50:
                                     prompt="What's another state name?").title()
 
     if answer_state == "Exit":
-        states_to_learn = []
-        for state in all_states:
-            if state not in guessed_states:
-                states_to_learn.append(state)
-        print(states_to_learn)
+        states_to_learn = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(states_to_learn)
         new_data.to_csv("states_to_learn.csv")
         break
